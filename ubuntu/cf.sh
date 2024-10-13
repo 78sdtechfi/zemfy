feef3add04062e54595ba15f56f4d3ebc13afdc3
78sdtechfi <gwadenyap@gmail.com>
2024-10-14 01:03:20 +0300

    Update cf.sh

--- ubuntu/cf.sh
+++ ubuntu/cf.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-MYIP=$(wget -qO- icanhazip.com);
-apt install jq curl -y
-#read -p "Masukan Domain (contoh : Dragon)" domen
-DOMAIN=domainmu
+
+# Set variables
+MYIP=$(wget -qO- icanhazip.com)
+DOMAIN="domainmu"
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.subdomainmu
-CF_ID=emailmu@gmail.com
-CF_KEY=keytele
-set -euo pipefail
-IP=$(wget -qO- icanhazip.com);
-echo "Updating DNS for ${dns}..."
+dns="${sub}.subdomainmu"
+CF_ID="emailmu@gmail.com"
+CF_KEY="keytele"
+
+# Install dependencies
+apt install jq curl -y
+
+# Set up Cloudflare API
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
@@ -20,23 +22,26 @@ RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
+# Create or update DNS record
 if [[ "${#RECORD}" -le 10 ]]; then
      RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+     --data '{"type":"A","name":"'${dns}'","content":"'${MYIP}'","ttl":120,"proxied":false}' | jq -r .result.id)
 fi
 
+# Update DNS record
 RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
+     --data '{"type":"A","name":"'${dns}'","content":"'${MYIP}'","ttl":120,"proxied":false}')
+
+# Write DNS record to files
 echo "$dns" > /root/domain
 echo "$dns" > /root/scdomain
 echo "$dns" > /etc/xray/domain
 echo "$dns" > /etc/v2ray/domain
 echo "$dns" > /etc/xray/scdomain
 echo "IP=$dns" > /var/lib/kyt/ipvps.conf
-cd
