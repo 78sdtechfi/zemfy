2ffc56fc67db2c5cf929cb55607c8ad124589ed9
78sdtechfi <gwadenyap@gmail.com>
2024-11-02 11:19:07 +0300

    update
    update 3.0.2

--- genz.sh
+++ genz.sh
@@ -278,7 +278,7 @@ restart_system(){
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m" 
 clear
-izinsc="https://raw.githubusercontent.com/78sdtechfi/zemfy/main/keygen"
+izinsc="https://raw.githubusercontent.com/78sdtechfi/genx/main/keygen"
 # USERNAME
 rm -f /usr/bin/user
 username=$(curl $izinsc | grep $MYIP | awk '{print $2}')
